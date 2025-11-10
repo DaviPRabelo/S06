f931a0c27427e955f6e7f10c1e84b4c5dd36de05
DaviPRabelo <davi.padula@gec.inatel.br>
2025-11-10 18:47:29 -0300

    fix: alterando aula 2

--- cypress/e2e/exAula2.cy.js
+++ cypress/e2e/exAula2.cy.js
@@ -1,5 +1,61 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe("Teste de criação, registro e login de usuário", () => {
+  it.skip("Deve criar um novo usuário, registrar e fazer login com sucesso", () => {
+    cy.visit("https://globalsqa.com/angularJs-protractor/registration-login-example/#/login")
+    cy.get('.btn-link').click()
+    cy.get('#firstName').type('Davi')
+    cy.get('#Text1').type('ivaD');
+    cy.get('#username').type('davi123')
+    cy.get('#password').type('12345')
+    cy.get('.btn-primary').click()
+    cy.get('.ng-binding').should("contain.text", "Registration successful")
+
+  })
+
+  it.skip("Deve criar um novo usuário, registrar e fazer login com  falhas", () => {
+    cy.visit("https://globalsqa.com/angularJs-protractor/registration-login-example/#/login")
+    cy.get('.btn-link').click()
+    cy.get('#firstName').type('Davi')
+    cy.get('#Text1').type('ivaD');
+    cy.get('#username').type('davi123')
+    cy.get('.btn-primary').should("be.disabled")
+
+  })
+  it("Teste de login função criarUsuario", () => {
+    let inf = criarUsuario()
+    cy.visit("https://globalsqa.com/angularJs-protractor/registration-login-example/#/login")
+    cy.get('#username').type(inf[0])
+    cy.get('#password').type(inf[3])
+    cy.get('.btn-primary').click()
+    cy.get('h1.ng-binding').should("contain.text", "Hi " + inf[0] + "!")
+  })
+  it("Delete usuario", () => {
+    let inf = criarUsuario()
+    cy.visit("https://globalsqa.com/angularJs-protractor/registration-login-example/#/login")
+    cy.get('#username').type(inf[0])
+    cy.get('#password').type(inf[3])
+    cy.get('.btn-primary').click()
+    cy.get('h1.ng-binding').should("contain.text", "Hi " + inf[0] + "!")
+    cy.get('.btn-danger').click()
+    cy.get('.ng-binding').should("contain.text", "User deleted.")
+
   })
 })
+
+function criarUsuario() {
+  let hr = new Date().getHours() + new Date().getMinutes();
+  let min = new Date().getMinutes();
+  let seg = new Date().getSeconds();
+  let usuario = "usr" + hr + min + seg;
+
+  let inf = [hr, min, seg, usuario];
+
+  cy.visit("https://globalsqa.com/angularJs-protractor/registration-login-example/#/login")
+  cy.get('.btn-link').click()
+  cy.get('#firstName').type(hr)
+  cy.get('#Text1').type(min);
+  cy.get('#username').type(seg)
+  cy.get('#password').type(usuario)
+  cy.get('.btn-primary').click()
+  cy.get('.ng-binding').should("contain.text", "Registration successful")
+  return inf;
+}
